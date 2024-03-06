8414d7de7e97a3951a4ee22b18c35490f74080fe
Johngibbard <162235543+Johngibbard@users.noreply.github.com>
2024-03-06 20:46:47 +0000

    added more comments

--- backend.py
+++ backend.py
@@ -1,4 +1,7 @@
 # This is intended to work as the backend, which will be writting in python
 # Todo John (Make login request search the SQL server to validate login)
 # Todo John (Make a Function with which to edit the person information in the SQL server)
+#   - Checks: 
+#           Check for invalid data inputs 
+#           Tell the User if one of there inputs is invalid
 #   - DEPENDANCY: REQUIRES SQL SERVER SETUP
