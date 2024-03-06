50a5933fbe5f6127eaf333c6e9fe228a70d70d7d
Johngibbard <162235543+Johngibbard@users.noreply.github.com>
2024-03-06 20:41:10 +0000

    Created the backend file for the project (might need to redo)

---
+++ backend.py
@@ -0,0 +1,4 @@
+# This is intended to work as the backend, which will be writting in python
+# Todo John (Make login request search the SQL server to validate login)
+# Todo John (Make a Function with which to edit the person information in the SQL server)
+#   - DEPENDANCY: REQUIRES SQL SERVER SETUP
