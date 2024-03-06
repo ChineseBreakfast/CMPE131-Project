9c4090b48a477a2d7ff5f86bdc9635a0035c515d
John <john.gibbard@sjsu.edi>
2024-03-06 14:50:42 -0800

    continued working on Login

--- Login.py
+++ Login.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.post('/')
-def index():
-    return render_template('test.html')
+@app.route('/', methods=['GET','POST'])
+def test():
+    return "<html> <body> <h1> you made it  </h1> </body> </html>"
