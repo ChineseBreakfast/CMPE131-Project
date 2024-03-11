1d973194e7b9e9b45577cb3ec7b3d77dcbf048cd
John <john.gibbard@sjsu.edi>
2024-03-10 19:12:35 -0700

    fixed request python

--- app.py
+++ app.py
@@ -11,19 +11,11 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/hello/<name>/<password>", )
-def hello_there(name,password):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now + "your password is" + password
-    return content
+@app.route('/login', methods = ["GET","POST"])
+def hello_there():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+# todo; once the password is collected, we need to check 
+# it against all of the other passwords in the database
+    return 
