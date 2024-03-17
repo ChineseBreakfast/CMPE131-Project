9ea1dc98f9a5640f8335b985c300edc53c25aeb1
John <john.gibbard@sjsu.edi>
2024-03-17 07:37:37 -0700

    working on data input

--- app.py
+++ app.py
@@ -16,13 +16,7 @@ class employee(db.Model):
     start_work = db.Column(db.types.Time())
     end_work = db.Column(db.types.Time())
     Password = db.Column(db.String(100),nullable = True)
-
-class group():
-    group_name = ""
-    employees = []
-    def __init__ (self,group_name,employees):
-        self.group_name = group_name
-        self.employees = employees
+ #   People = db.Column(db.PickleType, nullable = True)
 
 class meeting(db.Model):
     Meeting_id = db.Column(db.Integer, primary_key = True)
@@ -34,6 +28,17 @@ class meeting(db.Model):
 class room(db.Model):
     Room_number = db.Column(db.Integer, primary_key = True)
     meetings = db.Column(db.PickleType, nullable = True)
+
+class groupdb(db.Model):
+    group_id = db.Column(db.Integer, primary_key = True)
+    group_db = db.Column(db.PickleType, nullable = False)
+
+class group():
+    group_name = ""
+    employees = []
+    def __init__ (self,group_name,employees):
+        self.group_name = group_name
+        self.employees = employees
 
 @app.before_request
 def create_and_populate_db():
@@ -57,13 +62,13 @@ def hello_there():
     if request.method == "POST":
         if (employee.query.count() == 0):
             employees = [                
-            employee(name='Jim',  employee_id = '1', age=26, start_work = time(8,30,0,0), end_work= time(18,30,0,0), Password = 123),
-            employee(name='Jane', employee_id = '2', age=53, start_work = time(7,30,0,0),end_work= time(18,30,0,0), Password = 456),
-            employee(name='John', employee_id = '3', age=34, start_work = time(8,0,0,0), end_work= time(18,30,0,0),Password = 789)
+            employee(name='Jim Davis',  employee_id = '1', age=26, start_work = time(8,30,0,0), end_work= time(18,30,0,0), Password = 123),
+            employee(name='Jane Doe', employee_id = '2', age=53, start_work = time(7,30,0,0),end_work= time(18,30,0,0), Password = 456),
+            employee(name='John Dorne', employee_id = '3', age=34, start_work = time(8,0,0,0), end_work= time(18,30,0,0),Password = 789)
                         ]
             db.session.bulk_save_objects(employees)
             db.session.commit()
-        group1 = group("group1", [employee.query.get(1),employee.query.get(2),employee.query.get(3)])
+        # group1 = group("group1", [employee.query.get(1),employee.query.get(2),employee.query.get(3)])
         if (meeting.query.count() == 0):
             meetings = [
             meeting(Meeting_id = 1,Start = time(8,30,0,0) , End = time(11,30,0,0) ,Room_number = 1, People = group1),
@@ -78,11 +83,17 @@ def hello_there():
         for i in range(employee.query.count()):
             info = employee.query.get(i+1)
             if (info.name == username) and (info.Password == password):
-                return render_template('placeholder.html')
+                return render_template('input.html')
 
-    
+@app.route('/meeting_input', methods = ["GET","POST"])
+def main_menu():
+    if request.method == "POST":
+     #   Room_number = request.form.get("username")
+    #    password = request.form.get("password")
+
+        return render_template('input.html')
 # todo; once the password is collected, we need to check 
 # it against all of the other passwords in the database
-    return render_template('index.html')
+   
 if __name__ == '__main__':
     app.run(debug=True)
